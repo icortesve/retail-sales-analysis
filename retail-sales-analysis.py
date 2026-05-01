0617c8222d59b993feaf4c03729b7ecae5d7583c
icortesve <ivan.cortesve@gmail.com>
2026-04-30 21:52:46 -0400

    Agrega carpetas - Estructura delProyecto

--- retail-sales-analysis.py
+++ retail-sales-analysis.py
@@ -6,6 +6,14 @@ def cargar_datos(ruta_archivo):
     return datos
 
 if __name__ == "__main__":
-    ruta_archivo = '../data/retail_sales.csv'
+    ruta_archivo = r'C:\Users\ivanc\Desktop\2. Inversiones\Python_Clases\retail-sales-analysis\DATA\retail_sales_dataset.csv'
     datos = cargar_datos(ruta_archivo)
     print(datos)
+
+
+# Pedimos los must
+datos.shape
+datos.info()
+datos.describe()
+datos.head(5)
+datos.tail(5)
